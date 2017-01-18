a64dd586ee950f809ed7dc76795499e94c9de8b3
Jordi Condominas <jcondominas@gmail.com>
2017-01-18 12:21:10 +0100

    Add files via upload
    Add the python script to download the url of a gif from giphy

---
+++ search_gif.py
@@ -0,0 +1,19 @@
+import requests
+import sys
+import random
+import pyperclip
+
+PUBLIC_KEY = 'dc6zaTOxFJmzC'
+URL = 'http://api.giphy.com'
+SEARCH_PATH = '/v1/gifs/search'
+
+search_key = sys.argv[-1]
+payload = {'api_key': PUBLIC_KEY, 'q': search_key}
+r = requests.get(URL + SEARCH_PATH, params=payload)
+data = r.json()['data']
+array_size = len(data)
+image_position = random.randrange(0, array_size)
+image_url = data[image_position]['images']['original']['url']
+pyperclip.copy('![]('+image_url+')')
+print image_url + '\n'
+print 'Image url copied to your clipboard'
